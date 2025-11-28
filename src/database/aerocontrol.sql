15138fb478bea224a85eba855acee6fbfa25c604
Hiram GG <reaxorgg@gmail.com>
2025-11-27 19:36:38 -0600

    Base de datos definitiva por el momento Correr el sql de permisos despues de crearla

--- src/database/aerocontrol.sql
+++ src/database/aerocontrol.sql
@@ -140,33 +140,6 @@ REPLACE INTO `usuarios` (`id_usuario`, `nombre`, `usuario`, `password`, `rol`) V
     (2, 'Usuario General', 'usuario1', 'user123', 'UsuarioGeneral'),
     (3, 'Analista', 'analista1', 'analyst123', 'Analista');
    
---Permisos y creacion de roles
-
---Administrador General
-CREATE USER IF NOT EXISTS 'admin_aeropuerto'@'localhost' IDENTIFIED BY 'Admin123';
-GRANT ALL PRIVILEGES ON aerocontrol.* TO 'admin_aeropuerto'@'localhost' WITH GRANT OPTION;
-
---Analista
-CREATE USER IF NOT EXISTS 'analista'@'localhost' IDENTIFIED BY 'Analista123';
-GRANT SELECT ON aerocontrol.* TO 'analista'@'localhost';
-
---Usuario General
-CREATE USER IF NOT EXISTS 'usuario'@'localhost' IDENTIFIED BY 'Usuario123';
-
--- Ver vuelos
-GRANT SELECT ON aerocontrol.vuelo TO 'usuario'@'localhost';
-
--- Ver aeropuertos
-GRANT SELECT ON aerocontrol.aeropuerto TO 'usuario'@'localhost';
-
--- Ver pasajeros
-GRANT SELECT ON aerocontrol.pasajero TO 'usuario'@'localhost';
-
--- Registrar nuevos pasajeros
-GRANT INSERT ON aerocontrol.pasajero TO 'usuario'@'localhost';
-
--- Ver equipaje
-GRANT SELECT ON aerocontrol.equipaje TO 'usuario'@'localhost';
 
 -- Dumping structure for table aerocontrol.vuelo
 CREATE TABLE IF NOT EXISTS `vuelo` (
